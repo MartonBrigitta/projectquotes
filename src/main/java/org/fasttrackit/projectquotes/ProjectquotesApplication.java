71c333ca43435a76182cbc02feb523e9c01745e4
BRIGI MARTON <brigi.marton397@gmail.com>
2023-07-22 17:05:55 +0300

    Project quotes first commit

---
+++ src/main/java/org/fasttrackit/projectquotes/ProjectquotesApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.projectquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectquotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectquotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/projectquotes/ProjectquotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.projectquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectquotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
